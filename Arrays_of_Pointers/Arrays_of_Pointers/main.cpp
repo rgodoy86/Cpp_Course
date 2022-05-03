0dad6504239a08222947456c8272c3e980f0bd92
rgodoy86 <rgodoy86@gmail.com>
2022-05-03 12:41:09 +0200

    main.cpp updated.

--- Arrays_of_Pointers/Arrays_of_Pointers/main.cpp
+++ Arrays_of_Pointers/Arrays_of_Pointers/main.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 int main(void) {
+    // *** ARRAY OF INTS ***
     // Variables initialization
     int size = 10;
     Array *arr1 = new Array(size);
@@ -24,6 +25,31 @@ int main(void) {
     for(int i=0; i < size ; i++){
         cout << "Array[" << i << "]: " << arr1 -> get(i) << endl;
     }
+    
+    delete arr1;
+    cout << endl;
+    
+    // *** ARRAY OF ARRAYS ***
+    // Variables initialization
+    size = 2;
+    Array *arr_arrays[2] = {new Array(size), new Array(size) };
+    
+    // Assigning values
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            arr_arrays[i] -> set(j, 100 + (i+1)*10 + (j+3));
+        }
+    }
+        
+    // Printing values
+    for(int i=0; i < size ; i++){
+        for(int j = 0; j < size; j++){
+            cout << "Array[" << i << "] [" << j << "]: " << arr_arrays[i] -> get(j) << endl;
+        }
+    }
+    
+    delete arr_arrays[0];
+    delete arr_arrays[1];
     
     return 0;
 }
