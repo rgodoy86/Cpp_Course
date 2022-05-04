64e66b123464d6e549625fbbb905eef39ac2a1d5
rgodoy86 <rgodoy86@gmail.com>
2022-05-04 18:38:14 +0200

    Polymorphism class created.

---
+++ Polymorphism/Polymorphism/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Polymorphism
+//
+//  Created by Raphael Godoy on 04.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
